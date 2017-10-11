7baa0151a8b24ed987d33062d4cda2d4a788211b
vicols92 <victor.olsson92@hotmail.se>
2017-10-11 21:42:27 +0200

    Added active background color

--- myjs.js
+++ myjs.js
@@ -9,9 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
           if (panel.style.display === "block") {
               panel.style.display = "none";
               this.getElementsByTagName('span')[0].className = 'icon';
+              this.style.backgroundColor = '#efefef';
           } else {
               panel.style.display = "block";
               this.getElementsByTagName('span')[0].className = 'iconOpen';
+              this.style.backgroundColor = '#d4d2d2';
+
           }
       }
   }
